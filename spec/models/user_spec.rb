a97d7c25d68addca09b47ad427075c337bdc0c5f
Gabe Berke-Williams <gabebw@gabebw.com>
2016-01-23 22:32:27 -0800

    Remove unit tests for ActiveRecord associations
    * When associations work, feature specs pass. When they don't, feature specs
      crash spectacularly. It's difficult to accidentally write a feature spec that
      passes when associations are incorrect. Thus, remove unit tests for associations.
    * In contrast, validations can easily be missing when feature specs pass.
      Testing validations at the feature level is slow and error-prone. Thus, keep
      the unit tests for validations.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,11 +14,6 @@ RSpec.describe User do
     end
   end
 
-  context "associations" do
-    it { is_expected.to have_many(:recommended_items) }
-    it { is_expected.to have_many(:television_shows) }
-  end
-
   context "#recommend" do
     it "adds a television show to a user's recommendations" do
       television_show = create(:television_show)
