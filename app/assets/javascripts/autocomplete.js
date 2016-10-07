8f60b05eed70b5a5cc562f69abebacda2b1c48ce
Adarsh Pandit <adarshpandit@gmail.com>
2016-10-07 11:53:29 -0700

    wip

--- app/assets/javascripts/autocomplete.js
+++ app/assets/javascripts/autocomplete.js
@@ -1,20 +1,5 @@
-$(function() {
-
-  $("#television_show_title").autocomplete({
-    source: function(request, response) {
-      $.get(
-        "https://api.themoviedb.org/3/search/tv?api_key=e97858344ce2a799f9603fd8efd5f013",
-        { query: request.term },
-
-        function(data) {
-          response(data);
-        });
-    },
-
-    minLength: 2,
-
-    open: function(event, ui) {
-      $(".ui-autocomplete").css("z-index", 1000);
-    }
-  });
-});
+$("#television_show_title").autocomplete(
+  {
+    source: "https://api.themoviedb.org/3/search/tv?api_key=e97858344ce2a799f9603fd8efd5f013"
+  }
+);
